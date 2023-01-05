866a7d84a805d226ddfeffe843913c404310ca73
NikkyXO <alexisdame2017@gmail.com>
2023-01-05 15:50:09 +0100

    variable typing

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains the "make_multiplier" class
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) ->Callable[[float], float]:
+	"""The make_multiplier function"""
+	def mult(arg: float):
+		return multiplier * arg
+	return mult
